a8449fec6816cc12525e5fb078f13033dba67ef5
Samuuca1 <samuelchaves_2012@hotmail.com>
2025-08-02 18:50:20 -0400

    Just added a couple line to separate parts of the assignment

--- script.js
+++ script.js
@@ -13,14 +13,13 @@ let student = {
 console.log(student.age);
 console.log(student.name);
 student.basicinfo();
-
+console.log("\n");
 
 //Part 2 Json (Convert JSON into a JS Object)
 
 let jsonString = '{"name":"Samuel" , "age": 28, "enrolled": true, "courses":["Math", "Database", "WRIT120", "JavaScript"]}';
 let jsonJSObj = JSON.parse(jsonString);
 console.log(jsonJSObj.name);
-
 // Part 2 Json (Stringify)
 
 let student2 = {
@@ -30,3 +29,7 @@ let student2 = {
 
 let jsonStringify = JSON.stringify(student2);
 console.log(jsonStringify);
+console.log("\n");
+
+// Part 3 Destructuring 
+
